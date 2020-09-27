5c9d29064555db5e8849dbb12374d39d5d830f37
Yutaka <y.tajika529@gmail.com>
2020-09-27 22:26:34 +0900

    add configurations

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,18 +1,27 @@
 import Fluent
 import FluentMySQLDriver
 import Vapor
+import JWT
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(CORSMiddleware())
 
-    app.databases.use(.mysql(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .mysql)
+    guard let jwksString = Environment.process.JWKS else {
+        fatalError("No value was found at the given public key environment 'JWKS'")
+    }
+
+    guard let mysqlUrlString = Environment.process.MYSQL_CRED else {
+        fatalError("No value was found at the given public key environment 'MYSQL_CRED'")
+    }
+    guard let url = URL(string: mysqlUrlString) else {
+        fatalError("Cannot parse: \(mysqlUrlString) correctly")
+    }
+    app.databases.use(try DatabaseConfigurationFactory.mysql(url: url), as: .mysql)
+
+    try app.jwt.signers.use(jwksJSON: jwksString)
 
     app.migrations.add(CreateTodo())
 
